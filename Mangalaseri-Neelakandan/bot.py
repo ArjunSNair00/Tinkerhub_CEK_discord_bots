3b9b710bd775cb6121366817915cae9935b2c9f9
ArjunSNair00 <snairarjun43@gmail.com>
2026-01-13 20:33:20 +0530

    ping

--- Mangalaseri-Neelakandan/bot.py
+++ Mangalaseri-Neelakandan/bot.py
@@ -1,3 +1,21 @@
+from threading import Thread
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is alive!"
+
+def run():
+    app.run(host="0.0.0.0", port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+
+
 import os
 import discord
 from discord import app_commands
